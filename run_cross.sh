0fae9b9356c49ce0add339e3e8f9decefbe41ed4
lyushisyan <liushixian2019@gmail.com>
2026-03-17 21:09:49 +0300

    Stop tracking run_cross.sh

--- run_cross.sh
+++
@@ -1,4 +0,0 @@
-for n in 10 20 50 100 200 500 1000 2000 5000 10000; do
-    echo "Running input_cross_${n}nm.toml"
-    OMP_NUM_THREADS=36 ./build/ntmc "calculation-cross/input_cross_${n}nm.toml"
-done
